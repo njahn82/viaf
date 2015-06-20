17d26a615ba6385f9c7db05e9034df3c289a28e3
naja <najko.jahn@uni-bielefeld.de>
2015-06-20 14:06:59 +0200

    fix build issues

--- R/viaf_get.R
+++ R/viaf_get.R
@@ -7,6 +7,6 @@
 viaf_get <- function(viaf = NULL) {
   if(is.null(viaf))
     stop("No viaf provided")
-  tt_GET(path = paste0("viaf/", viaf), add_headers(Accept = "application/json+links")) %>%
+  tt_GET(path = paste0("viaf/", viaf), httr::add_headers(Accept = "application/json+links")) %>%
     tt_parse_js()
 }
